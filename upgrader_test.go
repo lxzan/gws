8f690c64ba28c7fe337656f8bdba7240ae6381aa
lixizan <lixizan@webull.com>
2023-01-16 18:11:23 +0800

    add unit tests

--- upgrader_test.go
+++ upgrader_test.go
@@ -27,16 +27,16 @@ func TestNoDelay(t *testing.T) {
 }
 
 func TestAccept(t *testing.T) {
-	var request = &http.Request{
-		Header: http.Header{},
-		Method: http.MethodGet,
-	}
 	var upgrader = NewUpgrader(
 		WithResponseHeader(http.Header{"Server": []string{"gws"}}),
 		WithEventHandler(new(webSocketMocker)),
 	)
 
 	t.Run("ok", func(t *testing.T) {
+		var request = &http.Request{
+			Header: http.Header{},
+			Method: http.MethodGet,
+		}
 		request.Header.Set("Connection", "Upgrade")
 		request.Header.Set("Upgrade", "websocket")
 		request.Header.Set("Sec-WebSocket-Version", "13")
@@ -46,12 +46,78 @@ func TestAccept(t *testing.T) {
 		assert.NoError(t, err)
 	})
 
-	t.Run("fail", func(t *testing.T) {
+	t.Run("fail Sec-WebSocket-Version", func(t *testing.T) {
+		var request = &http.Request{
+			Header: http.Header{},
+			Method: http.MethodGet,
+		}
 		request.Header.Set("Connection", "Upgrade")
 		request.Header.Set("Upgrade", "websocket")
 		request.Header.Set("Sec-WebSocket-Version", "14")
 		request.Header.Set("Sec-WebSocket-Key", "3tTS/Y+YGaM7TTnPuafHng==")
 		request.Header.Set("Sec-WebSocket-Extensions", "client_max_window_bits")
+		_, err := upgrader.Accept(newHttpWriter(), request)
+		assert.Error(t, err)
+	})
+
+	t.Run("fail method", func(t *testing.T) {
+		var request = &http.Request{
+			Header: http.Header{},
+			Method: http.MethodPost,
+		}
+		_, err := upgrader.Accept(newHttpWriter(), request)
+		assert.Error(t, err)
+	})
+
+	t.Run("fail Connection", func(t *testing.T) {
+		var request = &http.Request{
+			Header: http.Header{},
+			Method: http.MethodGet,
+		}
+		request.Header.Set("Connection", "up")
+		request.Header.Set("Upgrade", "websocket")
+		request.Header.Set("Sec-WebSocket-Version", "13")
+		_, err := upgrader.Accept(newHttpWriter(), request)
+		assert.Error(t, err)
+	})
+
+	t.Run("fail Connection", func(t *testing.T) {
+		var request = &http.Request{
+			Header: http.Header{},
+			Method: http.MethodGet,
+		}
+		request.Header.Set("Connection", "Upgrade")
+		request.Header.Set("Upgrade", "ws")
+		request.Header.Set("Sec-WebSocket-Version", "13")
+		_, err := upgrader.Accept(newHttpWriter(), request)
+		assert.Error(t, err)
+	})
+
+	t.Run("fail Sec-WebSocket-Key", func(t *testing.T) {
+		var request = &http.Request{
+			Header: http.Header{},
+			Method: http.MethodGet,
+		}
+		request.Header.Set("Connection", "Upgrade")
+		request.Header.Set("Upgrade", "websocket")
+		request.Header.Set("Sec-WebSocket-Version", "13")
+		_, err := upgrader.Accept(newHttpWriter(), request)
+		assert.Error(t, err)
+	})
+
+	t.Run("fail check origin", func(t *testing.T) {
+		upgrader.CheckOrigin = func(r *Request) bool {
+			return false
+		}
+		var request = &http.Request{
+			Header: http.Header{},
+			Method: http.MethodGet,
+		}
+		request.Header.Set("Connection", "Upgrade")
+		request.Header.Set("Upgrade", "websocket")
+		request.Header.Set("Sec-WebSocket-Version", "13")
+		request.Header.Set("Sec-WebSocket-Key", "3tTS/Y+YGaM7TTnPuafHng==")
+		request.Header.Set("Sec-WebSocket-Extensions", "permessage-deflate")
 		_, err := upgrader.Accept(newHttpWriter(), request)
 		assert.Error(t, err)
 	})
