13f0c5b6d43b7d2ffec4c20e7e480369be252929
lixizan <lixizan@webull.com>
2023-02-27 19:37:13 +0800

    unit tests

--- reader.go
+++ reader.go
@@ -49,13 +49,13 @@ func (c *Conn) readControl() error {
 }
 
 func (c *Conn) readMessage() error {
+	if atomic.LoadUint32(&c.closed) == 1 {
+		return internal.CloseNormalClosure
+	}
+
 	contentLength, err := c.fh.Parse(c.rbuf)
 	if err != nil {
 		return err
-	}
-
-	if atomic.LoadUint32(&c.closed) == 1 {
-		return internal.CloseNormalClosure
 	}
 
 	// RSV1, RSV2, RSV3:  1 bit each
